001d615fab031b37018932394f6508111aa3d3fb
Patrick Azu <azupatrick0@gmail.com>
2021-09-01 21:19:59 +0100

    create wiki commit

---
+++ src/tests/e2e/index.test.js
@@ -0,0 +1,48 @@
+import puppeteer from 'puppeteer';
+import takePageScreenShot from '../../utils/take-page-screenshot';
+require('dotenv').config();
+
+const options = {
+  headless: false,
+  executablePath: process.env.PUPPETEER_EXEC_PATH,
+}
+
+jest.setTimeout(200000);
+
+describe('Restcountries Test Suite', () => {
+  console.log('Test starting....')
+  let page;
+
+  let browser;
+
+  const homepage = 'https://restcountries-frontend.netlify.app/';
+  
+  beforeAll(async () => {
+    browser = await puppeteer.launch(options)
+    page = await browser.newPage();
+    await page.goto(homepage, { waitUntil: 'networkidle2' });
+  });
+
+  afterAll(async () => {
+    await page.waitForTimeout(20000);
+    await browser.close()
+  });
+
+  it('should visit the meeting link', async () => {
+    await page.waitForTimeout(10000);
+    expect(page.url()).toEqual('https://restcountries-frontend.netlify.app/');
+    await takePageScreenShot(page, 'landing2/1-homepage', false);
+  });
+
+  it('should search for a country', async () => {
+    const search = await page.waitForSelector("input[class='search__input']");
+    await search.click();
+    await search.type('Canada');
+    await page.waitForSelector('div[class="search"] div button');
+    await page.click('div[class="search"] div button');
+    await page.waitForTimeout(10000);
+    const countryName = await page.$eval('div[class="card__description"] div', e => e.innerHTML);
+    expect(countryName ).toEqual('Canada');
+    await takePageScreenShot(page, 'landing2/2-search', true);
+  });
+});
